3c314a97461bd032fe738f5da45e812843a72b59
Aditi K. <aditiknarayan@gmail.com>
2025-02-01 10:28:34 -0700

    single sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
+//works single sensor
 #include <Arduino.h>
-#include <Wire.h> // Include Wire.h - Arduino I2C library
-#include <SparkFunMLX90614.h> // Include IR thermometer library
+#include <Wire.h> //include Wire.h - Arduino I2C library
+#include <SparkFunMLX90614.h> //include IR thermometer library
 
-IRTherm temp; // Create an IRTherm object called temp
+IRTherm temp; //create IRTherm object called temp
 
 // put function declarations here:
 
@@ -11,27 +12,27 @@ IRTherm temp; // Create an IRTherm object called temp
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); // Initialize Serial to log output
-  Wire.begin(); //Joing I2C bus
+  Serial.begin(115200); //initialize Serial to log output
+  Wire.begin(); //join I2C bus
 
-  while (temp.begin() == false){ // Initialize thermal IR sensor
-    Serial.println("Qwiic IR thermometer did not acknowledge! Freezing!");
+  while (temp.begin() == false){ //initialize thermal IR sensor
+    Serial.println("IR thermometer did not acknowledge! Freezing!");
   }
-  Serial.println("Qwiic IR Thermometer did acknowledge.");
+  Serial.println("IR Thermometer did acknowledge.");
   
-  temp.setUnit(TEMP_F); // Set the library's units to Fahrenheit
-  // Alternatively, TEMP_F can be replaced with TEMP_C for Celsius or
-  // TEMP_K for Kelvin.
-  
-  pinMode(LED_BUILTIN, OUTPUT); // LED pin as output
+  temp.setUnit(TEMP_F); //set to farenheit
+  //TEMP_C for celsius
+  //TEMP_K for kelvin
+  //temp.setEmissivity(0.55);
+  pinMode(LED_BUILTIN, OUTPUT); //LED pin as output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //main code here to keep running:
   digitalWrite(LED_BUILTIN, HIGH);
     
-  //call temp.read() to read object and ambient temperatures from the sensor.
-  if (temp.read()) // On success, read() will return 1, on fail 0.
+  //read object and ambient temperatures from sensor
+  if (temp.read()) //on success return 1, on fail return 0
   {
 	//floats, calculated out to the unit you set with setUnit().
     Serial.print("Object: " + String(temp.object(), 2));
@@ -39,8 +40,8 @@ void loop() {
     Serial.print("Ambient: " + String(temp.ambient(), 2));
     Serial.println("F");
     Serial.println();
+    Serial.println("Emissivity: " + String(temp.readEmissivity(),3));
   }
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 }
-
